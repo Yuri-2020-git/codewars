2c65de53c920693221bfd7c6b6e9ea49db2cb865
litvinov <lotwinov@gmail.com>
2022-02-18 07:24:05 +0300

    new

---
+++ 7 star/Coding Meetup #5 - Higher-Order Functions Series - Is Ruby coming.js
@@ -0,0 +1,13 @@
+var list1 = [
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+    { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+    { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+    { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
+  ];
+
+function countLanguages(list) {
+    var count = {};
+    list.forEach(x => count[x.language] = (count[x.language] || 0) + 1);
+    return count;
+  }
+  console.log(countLanguages(list1));
---
+++ 7 star/Coding Meetup #6 - Higher-Order Functions Series - Can they code in the same language.js
@@ -0,0 +1,9 @@
+var list1 = [
+    { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+    { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+  ];
+  function isSameLanguage(list) {
+    return list.every(e => e.language === list[0].language);
+  }
+  console.log(isSameLanguage(list1));
