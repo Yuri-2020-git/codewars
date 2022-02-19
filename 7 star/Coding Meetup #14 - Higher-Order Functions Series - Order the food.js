47c491c6a2d3cae96956dec417a657014ef3a7ea
litvinov <lotwinov@gmail.com>
2022-02-19 10:51:54 +0300

    new

---
+++ 7 star/Coding Meetup #14 - Higher-Order Functions Series - Order the food.js
@@ -0,0 +1,17 @@
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C', 
+    meal: 'vegetarian' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript', 
+    meal: 'standard' },
+  { firstName: 'Ramona', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby', 
+    meal: 'vegan' },
+  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C', 
+    meal: 'vegetarian' },
+];
+
+function orderFood(list) {
+  var count = {};
+  list.forEach(x => count[x.meal] = (count[x.meal] || 0) + 1);
+  return count;
+}
+console.log(orderFood(list1));
