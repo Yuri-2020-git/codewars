32c0fc9462a7c896fa1a53f64c9b580ffa1e0d1c
litvinov <lotwinov@gmail.com>
2022-02-19 09:27:21 +0300

    new

---
+++ 7 star/Coding Meetup #10 - Higher-Order Functions Series - Create usernames.js
@@ -0,0 +1,16 @@
+var list1 = [
+    { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby' },
+    { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure' }
+  ];
+
+  /* [
+    { firstName: 'Emily', lastName: 'N.', country: 'Ireland', continent: 'Europe', age: 30, language: 'Ruby', 
+      username: 'emilyn1990' },
+    { firstName: 'Nor', lastName: 'E.', country: 'Malaysia', continent: 'Asia', age: 20, language: 'Clojure', 
+      username: 'nore2000' }
+  ] */
+
+  function addUsername(list) {
+    // thank you for checking out the Coding Meetup kata :)
+  }
+console.log()  
