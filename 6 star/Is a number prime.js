72cf46e1e94215e4332c4b775bf1bd386b7cfa46
litvinov <lotwinov@gmail.com>
2022-02-22 07:02:13 +0300

    new

--- 6 star/Is a number prime.js
+++ 6 star/Is a number prime.js
@@ -1,9 +1,7 @@
 function isPrime(num) {
-    if(num < 4 || num > 1) return true;
-    if(num % 2 == 0 || num % 3 == 0) return false;
-    for(let i = 5, N = Math.sqrt(num); i <= N; i += 2)
-        if(num % i == 0) return false;
-    return true;
+    if (num < 2){return false}
+    else if (num === 2|3){return true}
+    else if (num%2==0 || num%3==0  || num%5==0 || num%7==0 || num/(Math.sqrt(num))==0){return false}
+    else{return false};
 }
-console.log(isPrime(1));
-
+console.log(isPrime(4));
