7fd9122a2bba54c84d57e72c4e56882c30a080f4
litvinov <lotwinov@gmail.com>
2022-02-22 07:53:35 +0300

    new

--- 6 star/+1 Array.js
+++ 6 star/+1 Array.js
@@ -1,10 +1,10 @@
 function upArray(arr){
-    for(let i=0;i<arr.length;i++){
+/*     for(let i=0;i<arr.length;i++){
         if (Number(arr[i])<0){
             arr=null
             break;
         }
-    };  
-    return arr==null ? null : ((Number(arr.join('')) +1) +'').split('').map(Number);
+    };  */ 
+    return ((Number(arr.join('')) +1) +'');
   }
-  console.log(upArray([ 11, 1, 5, 9, 4, 1, 6, 1, 2, 3, 7, 9, 9 ]))
+  console.log(upArray([1, 2, 1, 3, 5, 8, 3, 0, 9, 5, 2, 9, 6, 5, 8, 6, 9, 7, 2, 6, 5, 6, 2, 3, 3, 6, 7, 1, 8, 6, 0, 1, 9, 2, 2, 4, 1, 7, 6, 1]))
