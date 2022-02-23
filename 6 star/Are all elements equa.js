f6f2f400cc613264be4a847b4f4f1b031f6656a1
litvinov <lotwinov@gmail.com>
2022-02-23 10:37:59 +0300

    new

--- 6 star/Are all elements equa.js
+++ 6 star/Are all elements equa.js
@@ -1,1 +1,11 @@
-console.log("aaaaa".eqAll());
+Object.defineProperty( Array.prototype, "eqAll", { value: function eqAll() {
+    return [...new Set(this)].length == 1 || [...new Set(this)].length == 0 ? true : false;
+  } } );
+
+Object.defineProperty( String.prototype, "eqAll", { value: function eqAll() {
+    return [...new Set(this.split(''))].length == 1 || [...new Set(this.split(''))].length == 0 ? true : false;      
+} } );
+
+//console.log([1].eqAll());
+console.log("12".eqAll());
+
