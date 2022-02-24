5125c67ac73afe825cc5c3a34ea613b0cbd0d4f1
litvinov <lotwinov@gmail.com>
2022-02-24 07:44:41 +0300

    new

--- 6 star/The Deaf Rats of Hamelin.js
+++ 6 star/The Deaf Rats of Hamelin.js
@@ -1,10 +1,10 @@
 var countDeafRats = function(town) {
     let arrLeft = town.split('P')[0]
-    let arrLeft1 = arrLeft.replace(/(~O)+( )/g, "");
+    let arrLeft1 = arrLeft.replace(/((~O)*)( *)(~*)/g, "");
 
     let arrRight = town.split('P')[1]
-    let arrRight1 = arrRight.replace(/(O~)+( )/g, "");
+    let arrRight1 = arrRight.replace(/((O~)*)( *)(~*)/g, "");
 
-    return arrRight1//.length;
+    return arrRight1.length;
   }
-console.log((countDeafRats("P O~ O~ ~O O~"))); //)1
+console.log((countDeafRats("~O~O~O~OP~O~OO~"))); //)1
