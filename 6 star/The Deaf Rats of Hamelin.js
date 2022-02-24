93faff04932f6a3cde96649ebfe852927709a9f8
litvinov <lotwinov@gmail.com>
2022-02-24 08:19:06 +0300

    new

--- 6 star/The Deaf Rats of Hamelin.js
+++ 6 star/The Deaf Rats of Hamelin.js
@@ -1,10 +1,22 @@
 var countDeafRats = function(town) {
-    let arrLeft = town.split('P')[0]
-    let arrLeft1 = arrLeft.replace(/((~O)*)( *)(~*)/g, "");
-
-    let arrRight = town.split('P')[1]
-    let arrRight1 = arrRight.replace(/((O~)*)( *)(~*)/g, "");
-
-    return arrRight1.length;
-  }
+  const [leftRats, rightRats] = town
+    .replace(/[^O~P]/g, '')
+    .split('P')
+    .map(segment => segment.match(/.{2}/g) || []);
+  return (
+    leftRats.reduce((a, rat) => a + (rat === 'O~'), 0) +
+    rightRats.reduce((a, rat) => a + (rat === '~O'), 0)
+  );
+}
 console.log((countDeafRats("~O~O~O~OP~O~OO~"))); //)1
+
+// var countDeafRats = function(town) {
+//   return [...town.replace(/ +/g,'')].reduce((a,c,i) => a+(i%2==0 && c=='O'), 0);
+// }
+
+// var countDeafRats = function(town) {
+//   const [a,b] = town.split('P')
+//   const l = (a.match(/(?<=^|\s|O)(O~)+/g)||[]).join('').length/2
+//   const r = (b.match(/(?<=^|\s|~)(~O)+/g)||[]).join('').length/2
+//   return l+r
+// }
